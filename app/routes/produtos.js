04472e4e2e557edca2a2f80915931e01cfbc6ddd
Rafael Romancini <rafael.romancini@gmail.com>
2018-02-20 21:57:00 -0300

    First commit

---
+++ app/routes/produtos.js
@@ -0,0 +1,12 @@
+var connectionFactory = require('../infra/connectionFactory');
+
+module.exports = function(app){
+    app.get('/produtos', function(req, res) {    
+        var connection = connectionFactory();    
+        connection.query('select * from livros', function (err, results) {
+            res.render('produtos/lista', {lista:results});
+        });
+        connection.end();
+    });
+}
+
